29dfbb5f9950d0817643c01c794fc8037e1d7f03
Whinery <61842355+Whinery@users.noreply.github.com>
2021-02-24 23:26:17 -0500

    Update hosts-updater.sh

--- hosts-updater.sh
+++ hosts-updater.sh
@@ -5,7 +5,6 @@
 ### *** ONLY REMOVE the "#" symbol from the BEGINNING of one of the 4 LINES BELOW! *** ###
           ### *** 0.0.0.0 version usually faster than 127.0.0.1 version *** ###
 
-sleep 10
 file0=/tmp/hostss                                                         # set file name for /tmp/hostss
 file1=/etc/hosts                                                          # set file name for /etc/hosts
 
@@ -14,7 +13,7 @@ curl https://someonewhocares.org/hosts/ipv6zero/hosts/ -s -o "$file0"     # Both
 # curl https://someonewhocares.org/hosts/ipv6/hosts -s -o "$file0"        # Both ipv4 and ipv6 -> 127.0.0.1 version
 # curl https://someonewhocares.org/hosts/hosts -s -o "$file0"             # ipv4 -> 127.0.0.1 -> 127.0.0.1 version
 
-
+sleep 5
 minimumsize=400000                                                        # set minimum file size in bytes
 actualsize=$(wc -c <"$file0")                                             # check actual size of hostss file
 if [ $actualsize -ge $minimumsize ]; then
