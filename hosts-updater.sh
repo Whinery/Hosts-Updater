3535b709366c5ea822e04a7af5054e7bbdea89d0
Whinery <61842355+Whinery@users.noreply.github.com>
2022-11-16 11:56:23 -0500

    Update hosts-updater.sh

--- hosts-updater.sh
+++ hosts-updater.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
+# Thanks to Steven Black for supplying and maintaining all the data at https://github.com/StevenBlack/hosts
 # Thanks to Dan Pollock for supplying and maintaining all the data at https://someonewhocares.org/hosts/ipv6/hosts
 
-### *** ONLY REMOVE the "#" symbol from the BEGINNING of one of the 4 LINES BELOW! *** ###
-          ### *** 0.0.0.0 version usually faster than 127.0.0.1 version *** ###
+### *** ONLY REMOVE the "#" symbol from the BEGINNING of one of the 5 LINES BELOW! *** ###
 
 file0=/tmp/hostss                                                         # set file name for /tmp/hostss
 file1=/etc/hosts                                                          # set file name for /etc/hosts
 file2=/etc/hosts.bak                                                      # set file name for /etc/hosts.bak
 
-curl https://someonewhocares.org/hosts/ipv6zero/hosts/ -s -o "$file0"     # Both ipv4 and ipv6 -> 0.0.0.0 version
-# curl https://someonewhocares.org/hosts/zero/hosts -s -o "$file0"        # ipv4 -> 0.0.0.0 version
-# curl https://someonewhocares.org/hosts/ipv6/hosts -s -o "$file0"        # Both ipv4 and ipv6 -> 127.0.0.1 version
-# curl https://someonewhocares.org/hosts/hosts -s -o "$file0"             # ipv4 -> 127.0.0.1 -> 127.0.0.1 version
+curl https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts -s -o "$file0"    && minimumsize=4000000       # adware + malware
+# curl https://someonewhocares.org/hosts/ipv6zero/hosts/ -s -o "$file0"			&& minimumsize=400000        # Both ipv4 and ipv6 -> 0.0.0.0 version
+# curl https://someonewhocares.org/hosts/zero/hosts -s -o "$file0"			&& minimumsize=400000        # ipv4 -> 0.0.0.0 version
+# curl https://someonewhocares.org/hosts/ipv6/hosts -s -o "$file0"			&& minimumsize=400000        # Both ipv4 and ipv6 -> 127.0.0.1 version
+# curl https://someonewhocares.org/hosts/hosts -s -o "$file0"				&& minimumsize=400000        # ipv4 -> 127.0.0.1 -> 127.0.0.1 version
 
 sleep 5
-minimumsize=400000                                                        # set minimum file size in bytes
 actualsize=$(wc -c <"$file0")                                             # check actual size of hostss file
 if [ $actualsize -ge $minimumsize ]; then
 	rm -f "$file1"                                                    # remove /etc/hosts file
